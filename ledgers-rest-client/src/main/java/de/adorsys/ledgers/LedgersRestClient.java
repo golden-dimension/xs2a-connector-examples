6d4ff3077d26ae637e2cfc9d8e681292d60d498d
fpo <fpo@adorsys.de>
2018-11-25 16:44:39 +0100

    Modifications due to improvements on ledgers Middleware interface

--- ledgers-rest-client/src/main/java/de/adorsys/ledgers/LedgersRestClient.java
+++ ledgers-rest-client/src/main/java/de/adorsys/ledgers/LedgersRestClient.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "ledgers", url = "${ledgers.url}")
 public interface LedgersRestClient {
