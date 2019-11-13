ec2cf0087efcc1b9642eb33545f9bfb8f2d3a21a
Serhii Shymkov <ssh@adorsys.com.ua>
2019-11-13 18:40:54 +0200

    Remove unneeded Xs2aInterfaceConfig classes

--- xs2a-connector-embedded/src/main/java/de/adorsys/aspsp/xs2a/embedded/connector/LedgersXS2AConnectorEmbeddedConfiguration.java
+++ xs2a-connector-embedded/src/main/java/de/adorsys/aspsp/xs2a/embedded/connector/LedgersXS2AConnectorEmbeddedConfiguration.java
@@ -1,15 +1,11 @@
 package de.adorsys.aspsp.xs2a.embedded.connector;
 
 import de.adorsys.aspsp.xs2a.connector.EnableLedgersXS2AConnector;
-import de.adorsys.aspsp.xs2a.embedded.connector.config.EmbeddedXs2aConfig;
-import de.adorsys.aspsp.xs2a.embedded.connector.spi.web.Xs2aInterfaceConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan
 @EnableLedgersXS2AConnector
-@Import({Xs2aInterfaceConfig.class, EmbeddedXs2aConfig.class})
 public class LedgersXS2AConnectorEmbeddedConfiguration {
 }
