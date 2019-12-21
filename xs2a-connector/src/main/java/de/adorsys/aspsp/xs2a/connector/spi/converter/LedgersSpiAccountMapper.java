fc1f59e055fbc1f83cc129f7568ab9ac0a355f7b
romanyelizarov <rye@adorsys.com.ua>
2019-12-21 13:12:35 +0200

    refactored mapper

--- xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/converter/LedgersSpiAccountMapper.java
+++ xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/converter/LedgersSpiAccountMapper.java
@@ -115,6 +115,10 @@ public abstract class LedgersSpiAccountMapper {
     public abstract FundsConfirmationRequestTO toFundsConfirmationTO(SpiPsuData psuData, SpiFundsConfirmationRequest spiFundsConfirmationRequest);
 
     private String mapRemittanceInformationToString(RemittanceInformationStructuredTO remittanceInformationStructuredTO) {
+        if (remittanceInformationStructuredTO == null) {
+            return null;
+        }
+
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             return objectMapper.writeValueAsString(remittanceInformationStructuredTO);
