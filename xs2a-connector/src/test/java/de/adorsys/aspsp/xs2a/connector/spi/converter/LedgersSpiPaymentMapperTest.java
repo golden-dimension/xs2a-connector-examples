e047398975d542546c5af6967cd302451e6e9160
ihorkuchevskiy <iku@adorsys.com.ua>
2020-01-30 18:17:17 +0200

    added tests for mapping

--- xs2a-connector/src/test/java/de/adorsys/aspsp/xs2a/connector/spi/converter/LedgersSpiPaymentMapperTest.java
+++ xs2a-connector/src/test/java/de/adorsys/aspsp/xs2a/connector/spi/converter/LedgersSpiPaymentMapperTest.java
@@ -3,16 +3,11 @@ package de.adorsys.aspsp.xs2a.connector.spi.converter;
 import de.adorsys.aspsp.xs2a.util.JsonReader;
 import de.adorsys.ledgers.middleware.api.domain.payment.BulkPaymentTO;
 import de.adorsys.ledgers.middleware.api.domain.payment.PaymentProductTO;
-import de.adorsys.psd2.xs2a.core.pis.FrequencyCode;
-import de.adorsys.psd2.xs2a.core.pis.PisDayOfExecution;
-import de.adorsys.psd2.xs2a.core.pis.PisExecutionRule;
-import de.adorsys.psd2.xs2a.core.pis.TransactionStatus;
+import de.adorsys.ledgers.middleware.api.domain.payment.PaymentTO;
+import de.adorsys.psd2.xs2a.core.pis.*;
 import de.adorsys.psd2.xs2a.spi.domain.account.SpiAccountReference;
 import de.adorsys.psd2.xs2a.spi.domain.common.SpiAmount;
-import de.adorsys.psd2.xs2a.spi.domain.payment.SpiAddress;
-import de.adorsys.psd2.xs2a.spi.domain.payment.SpiBulkPayment;
-import de.adorsys.psd2.xs2a.spi.domain.payment.SpiPeriodicPayment;
-import de.adorsys.psd2.xs2a.spi.domain.payment.SpiSinglePayment;
+import de.adorsys.psd2.xs2a.spi.domain.payment.*;
 import de.adorsys.psd2.xs2a.spi.domain.payment.response.SpiBulkPaymentInitiationResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -57,38 +52,53 @@ class LedgersSpiPaymentMapperTest {
         assertNull(actualResult);
     }
 
+    @Test
+    void toPaymentTO_Single() {
+        // Given
+        PaymentTO expectedResult = jsonReader.getObjectFromFile("json/mappers/single-payment-Initiation.json", PaymentTO.class);
+        SpiSinglePayment spiSinglePayment = updateSpiPayment(getSpiSingle());
+
+        // When
+        PaymentTO actualResult = ledgersSpiPaymentMapper.mapToPaymentTO(spiSinglePayment);
+
+        // Then
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void toPaymentTO_Bulk() {
+        // Given
+        PaymentTO expectedResult = jsonReader.getObjectFromFile("json/mappers/bulk-payment-Initiation.json", PaymentTO.class);
+        SpiBulkPayment spiBulkPayment = getBulk();
+        spiBulkPayment.getPayments().forEach(this::updateSpiPayment);
+
+        // When
+        PaymentTO actualResult = ledgersSpiPaymentMapper.mapToPaymentTO(spiBulkPayment);
+
+        // Then
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void toPaymentTO_Periodic() {
+        // Given
+        PaymentTO expectedResult = jsonReader.getObjectFromFile("json/mappers/periodic-payment-Initiation.json", PaymentTO.class);
+        SpiPeriodicPayment spiPeriodicPayment = updateSpiPayment(getPeriodic());
+
+        // When
+        PaymentTO actualResult = ledgersSpiPaymentMapper.mapToPaymentTO(spiPeriodicPayment);
+
+        // Then
+        assertEquals(expectedResult, actualResult);
+    }
+
     private SpiSinglePayment getSpiSingle() {
         SpiSinglePayment spiPayment = new SpiSinglePayment(PAYMENT_PRODUCT_SEPA.getValue());
-        spiPayment.setPaymentId("myPaymentId");
-        spiPayment.setEndToEndIdentification("123456789");
-        spiPayment.setDebtorAccount(getSpiAccountReference());
-        spiPayment.setInstructedAmount(new SpiAmount(Currency.getInstance("EUR"), BigDecimal.valueOf(100)));
-        spiPayment.setCreditorAccount(getSpiAccountReference());
-        spiPayment.setCreditorAgent("agent");
-        spiPayment.setCreditorName("Rozetka.ua");
-        spiPayment.setCreditorAddress(new SpiAddress("SomeStreet", "666", "Kiev", "04210", "Ukraine"));
-        spiPayment.setRemittanceInformationUnstructured("remittance");
-        spiPayment.setPaymentStatus(TransactionStatus.RCVD);
-        spiPayment.setRequestedExecutionDate(LocalDate.of(2018, 12, 12));
-        spiPayment.setRequestedExecutionTime(OffsetDateTime.of(LocalDate.of(2018, 12, 12), LocalTime.of(12, 0), ZoneOffset.UTC));
-        return spiPayment;
+        return fillSpiPaymentWithInitialData(spiPayment);
     }
 
     private SpiPeriodicPayment getPeriodic() {
-        SpiPeriodicPayment spiPayment = new SpiPeriodicPayment(PAYMENT_PRODUCT_SEPA.getValue());
-        spiPayment.setPaymentId("myPaymentId");
-        spiPayment.setEndToEndIdentification("123456789");
-        spiPayment.setDebtorAccount(getSpiAccountReference());
-        spiPayment.setInstructedAmount(new SpiAmount(Currency.getInstance("EUR"), BigDecimal.valueOf(100)));
-        spiPayment.setCreditorAccount(getSpiAccountReference());
-        spiPayment.setCreditorAgent("agent");
-        spiPayment.setCreditorName("Rozetka.ua");
-        spiPayment.setCreditorAddress(new SpiAddress("SomeStreet", "666", "Kiev", "04210", "Ukraine"));
-        spiPayment.setRemittanceInformationUnstructured("remittance");
-        spiPayment.setPaymentStatus(TransactionStatus.RCVD);
-        spiPayment.setRequestedExecutionDate(LocalDate.of(2018, 12, 12));
-        spiPayment.setRequestedExecutionTime(OffsetDateTime.of(LocalDate.of(2018, 12, 12), LocalTime.of(12, 0), ZoneOffset.UTC));
-
+        SpiPeriodicPayment spiPayment = fillSpiPaymentWithInitialData(new SpiPeriodicPayment(PAYMENT_PRODUCT_SEPA.getValue()));
         spiPayment.setStartDate(LocalDate.of(2018, 12, 12));
         spiPayment.setEndDate(LocalDate.of(2018, 12, 28));
         spiPayment.setExecutionRule(PisExecutionRule.FOLLOWING);
@@ -107,19 +117,47 @@ class LedgersSpiPaymentMapperTest {
         SpiSinglePayment one = getSpiSingle();
         one.setPaymentId("myPaymentId1");
         one.setEndToEndIdentification("123456788");
-        one.setCreditorAccount(getSpiAccountReference());
         one.setInstructedAmount(new SpiAmount(Currency.getInstance("EUR"), BigDecimal.valueOf(200)));
-        one.setCreditorAccount(getSpiAccountReference());
 
         SpiSinglePayment two = getSpiSingle();
         two.setPaymentId("myPaymentId2");
-        two.setCreditorAccount(getSpiAccountReference());
         two.setCreditorName("Sokol.ua");
         two.setPaymentProduct(PAYMENT_PRODUCT_CROSS_BORDER.getValue());
 
         payment.setPayments(Arrays.asList(one, two));
         payment.setPaymentProduct(PAYMENT_PRODUCT_SEPA.getValue());
         return payment;
+    }
+
+    private <T extends SpiSinglePayment> T updateSpiPayment(T payment) {
+        SpiRemittance spiRemittance = buildSpiRemittance();
+        payment.setRemittanceInformationStructured(spiRemittance);
+        payment.setPurposeCode(PurposeCode.BKDF);
+        return payment;
+    }
+
+    private <T extends SpiSinglePayment> T fillSpiPaymentWithInitialData(T payment) {
+        payment.setPaymentId("myPaymentId");
+        payment.setEndToEndIdentification("123456789");
+        payment.setDebtorAccount(getSpiAccountReference());
+        payment.setInstructedAmount(new SpiAmount(Currency.getInstance("EUR"), BigDecimal.valueOf(100)));
+        payment.setCreditorAccount(getSpiAccountReference());
+        payment.setCreditorAgent("agent");
+        payment.setCreditorName("Rozetka.ua");
+        payment.setCreditorAddress(new SpiAddress("SomeStreet", "666", "Kiev", "04210", "Ukraine"));
+        payment.setRemittanceInformationUnstructured("remittance");
+        payment.setPaymentStatus(TransactionStatus.RCVD);
+        payment.setRequestedExecutionDate(LocalDate.of(2018, 12, 12));
+        payment.setRequestedExecutionTime(OffsetDateTime.of(LocalDate.of(2018, 12, 12), LocalTime.of(12, 0), ZoneOffset.UTC));
+        return payment;
+    }
+
+    private SpiRemittance buildSpiRemittance() {
+        SpiRemittance spiRemittance = new SpiRemittance();
+        spiRemittance.setReference("Ref Number Merchant");
+        spiRemittance.setReferenceIssuer("reference issuer");
+        spiRemittance.setReferenceType("reference type");
+        return spiRemittance;
     }
 
     private SpiAccountReference getSpiAccountReference() {
