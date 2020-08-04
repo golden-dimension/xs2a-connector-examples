a5a8997140be690b3ebed00e1a14122b7c19e69c
ihorkuchevskiy <iku@adorsys.com.ua>
2020-08-04 12:11:38 +0300

    #951 Implement multilevel for PIIS consents

--- xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/impl/PiisConsentSpiImpl.java
+++ xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/impl/PiisConsentSpiImpl.java
@@ -16,9 +16,33 @@
 
 package de.adorsys.aspsp.xs2a.connector.spi.impl;
 
+import de.adorsys.psd2.xs2a.core.profile.AccountReference;
+import de.adorsys.psd2.xs2a.spi.domain.SpiAspspConsentDataProvider;
+import de.adorsys.psd2.xs2a.spi.domain.SpiContextData;
+import de.adorsys.psd2.xs2a.spi.domain.account.SpiAccountReference;
+import de.adorsys.psd2.xs2a.spi.domain.consent.SpiInitiatePiisConsentResponse;
+import de.adorsys.psd2.xs2a.spi.domain.piis.SpiPiisConsent;
+import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponse;
 import de.adorsys.psd2.xs2a.spi.service.PiisConsentSpi;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+
 @Component
+@RequiredArgsConstructor
 public class PiisConsentSpiImpl implements PiisConsentSpi {
+    private final MultilevelScaService multilevelScaService;
+
+    @Override
+    public SpiResponse<SpiInitiatePiisConsentResponse> initiatePiisConsent(@NotNull SpiContextData contextData, SpiPiisConsent spiPiisConsent, @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
+        AccountReference account = spiPiisConsent.getAccount();
+        SpiInitiatePiisConsentResponse spiInitiatePiisConsentResponse = new SpiInitiatePiisConsentResponse();
+        SpiAccountReference spiAccountReference = new SpiAccountReference(null, account.getIban(), account.getBban(), account.getPan(), account.getMaskedPan(), account.getMsisdn(), account.getCurrency());
+        spiInitiatePiisConsentResponse.setSpiAccountReference(spiAccountReference);
+        boolean multilevelScaRequired = multilevelScaService.isMultilevelScaRequired(contextData.getPsuData(), Collections.singleton(spiAccountReference));
+        spiInitiatePiisConsentResponse.setMultilevelScaRequired(multilevelScaRequired);
+        return SpiResponse.<SpiInitiatePiisConsentResponse>builder().payload(spiInitiatePiisConsentResponse).build();
+    }
 }
