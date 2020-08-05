c8e087e40bbc07643a05bbffdfd8e4397e42d710
kyaex <kya@adorsys.com.ua>
2020-08-05 17:38:12 +0300

    #952 Support Funds Confirmation Consent in Embedded approach with multilevel sca

--- xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/impl/PiisConsentSpiImpl.java
+++ xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/impl/PiisConsentSpiImpl.java
@@ -16,15 +16,21 @@
 
 package de.adorsys.aspsp.xs2a.connector.spi.impl;
 
+import de.adorsys.psd2.xs2a.core.authorisation.AuthenticationObject;
+import de.adorsys.psd2.xs2a.core.consent.ConsentStatus;
 import de.adorsys.psd2.xs2a.core.error.MessageErrorCode;
 import de.adorsys.psd2.xs2a.core.error.TppMessage;
-import de.adorsys.psd2.xs2a.core.profile.AccountReference;
+import de.adorsys.psd2.xs2a.core.sca.ChallengeData;
+import de.adorsys.psd2.xs2a.core.sca.ScaStatus;
 import de.adorsys.psd2.xs2a.spi.domain.SpiAspspConsentDataProvider;
 import de.adorsys.psd2.xs2a.spi.domain.SpiContextData;
-import de.adorsys.psd2.xs2a.spi.domain.account.SpiAccountReference;
+import de.adorsys.psd2.xs2a.spi.domain.authorisation.*;
+import de.adorsys.psd2.xs2a.spi.domain.consent.SpiConsentConfirmationCodeValidationResponse;
 import de.adorsys.psd2.xs2a.spi.domain.consent.SpiConsentStatusResponse;
 import de.adorsys.psd2.xs2a.spi.domain.consent.SpiInitiatePiisConsentResponse;
+import de.adorsys.psd2.xs2a.spi.domain.consent.SpiVerifyScaAuthorisationResponse;
 import de.adorsys.psd2.xs2a.spi.domain.piis.SpiPiisConsent;
+import de.adorsys.psd2.xs2a.spi.domain.psu.SpiPsuData;
 import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponse;
 import de.adorsys.psd2.xs2a.spi.service.PiisConsentSpi;
 import feign.FeignException;
@@ -33,30 +39,116 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class PiisConsentSpiImpl implements PiisConsentSpi {
+    @Override
+    public SpiResponse<SpiPsuAuthorisationResponse> authorisePsu(@NotNull SpiContextData contextData, @NotNull String authorisationId, @NotNull SpiPsuData psuLoginData, String password, SpiPiisConsent businessObject, @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
+        log.info("PiisConsentSpiImpl#authorisePsu: contextData {}, psuLoginData {}, businessObject-id {}", contextData, psuLoginData, businessObject.getId());
+
+        return SpiResponse.<SpiPsuAuthorisationResponse>builder()
+                       .payload(new SpiPsuAuthorisationResponse(false, SpiAuthorisationStatus.SUCCESS))
+                       .build();
+    }
+
+    @Override
+    public SpiResponse<SpiAvailableScaMethodsResponse> requestAvailableScaMethods(@NotNull SpiContextData contextData, SpiPiisConsent businessObject, @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
+        log.info("PiisConsentSpiImpl#requestAvailableScaMethods: contextData {}, businessObject-id {}", contextData, businessObject.getId());
+
+        List<AuthenticationObject> spiScaMethods = new ArrayList<>();
+        AuthenticationObject sms = new AuthenticationObject();
+        sms.setAuthenticationType("SMS_OTP");
+        sms.setAuthenticationMethodId("sms");
+        sms.setName("some-sms-name");
+        spiScaMethods.add(sms);
+        AuthenticationObject push = new AuthenticationObject();
+        push.setAuthenticationType("PUSH_OTP");
+        push.setAuthenticationMethodId("push");
+        push.setDecoupled(true);
+        spiScaMethods.add(push);
+
+        return SpiResponse.<SpiAvailableScaMethodsResponse>builder()
+                       .payload(new SpiAvailableScaMethodsResponse(false, spiScaMethods))
+                       .build();
+    }
+
+    @Override
+    public @NotNull SpiResponse<SpiAuthorizationCodeResult> requestAuthorisationCode(@NotNull SpiContextData contextData, @NotNull String authenticationMethodId, @NotNull SpiPiisConsent businessObject, @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
+        log.info("PiisConsentSpiImpl#requestAuthorisationCode: contextData {}, authenticationMethodId {}, businessObject-id {}", contextData, authenticationMethodId, businessObject.getId());
+
+        SpiAuthorizationCodeResult spiAuthorizationCodeResult = new SpiAuthorizationCodeResult();
+        AuthenticationObject method = new AuthenticationObject();
+        method.setAuthenticationMethodId("sms");
+        method.setAuthenticationType("SMS_OTP");
+        spiAuthorizationCodeResult.setSelectedScaMethod(method);
+        spiAuthorizationCodeResult.setChallengeData(new ChallengeData(null, Collections.singletonList("some data"), "some link", 100, null, "info"));
+
+        return SpiResponse.<SpiAuthorizationCodeResult>builder()
+                       .payload(spiAuthorizationCodeResult)
+                       .build();
+    }
+
+    @Override
+    public @NotNull SpiResponse<Boolean> requestTrustedBeneficiaryFlag(@NotNull SpiContextData contextData, @NotNull SpiPiisConsent businessObject, @NotNull String authorisationId, @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
+        log.info("PiisConsentSpiImpl#requestTrustedBeneficiaryFlag: contextData {}, businessObject-id {}", contextData, businessObject.getId());
+
+        return SpiResponse.<Boolean>builder()
+                       .payload(true)
+                       .build();
+    }
+
+    @Override
+    public @NotNull SpiResponse<SpiVerifyScaAuthorisationResponse> verifyScaAuthorisation(@NotNull SpiContextData contextData, @NotNull SpiScaConfirmation spiScaConfirmation, @NotNull SpiPiisConsent spiPiisConsent, @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
+        log.info("PiisConsentSpiImpl#verifyScaAuthorisation: contextData {}, spiScaConfirmation {}, consent-id {}", contextData, spiScaConfirmation, spiPiisConsent.getId());
+
+        return SpiResponse.<SpiVerifyScaAuthorisationResponse>builder()
+                       .payload(new SpiVerifyScaAuthorisationResponse(ConsentStatus.VALID))
+                       .build();
+    }
+
+    @Override
+    public @NotNull SpiResponse<SpiConsentConfirmationCodeValidationResponse> notifyConfirmationCodeValidation
+            (@NotNull SpiContextData spiContextData, boolean confirmationCodeValidationResult,
+             @NotNull SpiPiisConsent spiPiisConsent, @NotNull SpiAspspConsentDataProvider spiAspspConsentDataProvider) {
+        ScaStatus scaStatus = confirmationCodeValidationResult ? ScaStatus.FINALISED : ScaStatus.FAILED;
+        ConsentStatus consentStatus = confirmationCodeValidationResult ? ConsentStatus.VALID : ConsentStatus.REJECTED;
+
+        SpiConsentConfirmationCodeValidationResponse response = new SpiConsentConfirmationCodeValidationResponse(scaStatus, consentStatus);
+
+        return SpiResponse.<SpiConsentConfirmationCodeValidationResponse>builder()
+                       .payload(response)
+                       .build();
+    }
+
+    @Override
+    public @NotNull SpiResponse<SpiConsentConfirmationCodeValidationResponse> checkConfirmationCode
+            (@NotNull SpiContextData spiContextData,
+             @NotNull SpiCheckConfirmationCodeRequest spiCheckConfirmationCodeRequest,
+             @NotNull SpiAspspConsentDataProvider spiAspspConsentDataProvider) {
+        return SpiResponse.<SpiConsentConfirmationCodeValidationResponse>builder()
+                       .payload(new SpiConsentConfirmationCodeValidationResponse(ScaStatus.FINALISED, ConsentStatus.VALID))
+                       .build();
+    }
     private final MultilevelScaService multilevelScaService;
 
     @Override
     public SpiResponse<SpiInitiatePiisConsentResponse> initiatePiisConsent(@NotNull SpiContextData contextData, SpiPiisConsent spiPiisConsent, @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
-        AccountReference account = spiPiisConsent.getAccount();
         SpiInitiatePiisConsentResponse spiInitiatePiisConsentResponse = new SpiInitiatePiisConsentResponse();
-        SpiAccountReference spiAccountReference = new SpiAccountReference(null, account.getIban(), account.getBban(), account.getPan(), account.getMaskedPan(), account.getMsisdn(), account.getCurrency());
-        spiInitiatePiisConsentResponse.setSpiAccountReference(spiAccountReference);
+        spiInitiatePiisConsentResponse.setSpiAccountReference(spiPiisConsent.getAccount());
         boolean multilevelScaRequired;
         try {
-            multilevelScaRequired = multilevelScaService.isMultilevelScaRequired(contextData.getPsuData(), Collections.singleton(spiAccountReference));
+            multilevelScaRequired = multilevelScaService.isMultilevelScaRequired(contextData.getPsuData(), Collections.singleton(spiPiisConsent.getAccount()));
         } catch (FeignException e) {
             log.error("Error during REST call for consent initiation to ledgers for account multilevel checking, PSU ID: {}", contextData.getPsuData().getPsuId());
             return SpiResponse.<SpiInitiatePiisConsentResponse>builder()
                            .error(new TppMessage(MessageErrorCode.FORMAT_ERROR_UNKNOWN_ACCOUNT))
                            .build();
         }
-
         spiInitiatePiisConsentResponse.setMultilevelScaRequired(multilevelScaRequired);
         return SpiResponse.<SpiInitiatePiisConsentResponse>builder().payload(spiInitiatePiisConsentResponse).build();
     }
